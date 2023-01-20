95737ca9cf48c945f658ea6637d32e94d3da522b
juanlendaro <juanlendaro@gmail.com>
2023-01-20 13:29:19 -0500

    manejo de archivos con fs

--- clase5/clase5.js
+++ clase5/clase5.js
@@ -1,45 +1,97 @@
+const fs = require('fs')
+
 class ProductManager {
-    constructor() {
+    constructor(path) {
         this.products = []
+        this.path = path
     }
 
-    addProduct = (title, description, price, thumbnail, code, stock) => {
-        const product = {
-            id: this.#getMaxId() + 1,
-            title,
-            description,
-            price,
-            thumbnail,
-            code,
-            stock,
+
+    addProduct = async (newProduct) => {
+        if (newProduct.title === '' || newProduct.description === '' || newProduct.price === '' || newProduct.thumbnail === '' || newProduct.code === '' || newProduct.stock === '') {
+            return console.log(`Complete todos los campos por favor`)
         }
-        this.products.push(product)
-        console.log(this.products)
+        let productDb = await this.getProducts()
+        const data = await productDb.find(product => product.code === newProduct.code)
+        try {
+            if (data) {
+                return console.log(`El código de producto ya existe`)
+            }
+            if (productDb.length === 0) {
+                newProduct.id = 1
+                productDb.push(newProduct)
+            } else {
+                productDb = [...productDb, { ...newProduct, id: productDb[productDb.length - 1].id + 1 }]
+            }
+            fs.promises.writeFile(this.path, JSON.stringify(productDb, null, '\t'))
+            console.log('Producto agregado');
+        } catch (error) {
+            console.log(error);
+        }
+
     }
-    #getMaxId() {
-        let maxId = 0
-        this.products.map((product) => {
-            if (product.id > maxId) maxId = product.id
-        })
-        return maxId
+    getProducts = async () => {
+        try {
+            if (fs.existsSync(this.path)) {
+                const data = await fs.promises.readFile(this.path, 'utf-8')
+                const productDb = JSON.parse(data);
+                return productDb;
+            }
+            await fs.promises.writeFile(this.path, '[]', 'utf-8')
+            return []
+        } catch (error) {
+            console.log(error);
+        }
     }
 
-    getProducts() {
-        return this.products
-
+    getProductById = async (id) => {
+        const data = await fs.promises.readFile(this.path, 'utf-8')
+        const productDb = JSON.parse(data).find(product => product.id === id)
+        if (!productDb) {
+            return console.log(`No existe producto con el id: ${id}`)
+        }
+        return console.log(productDb)
     }
-
-    getProductById = (productId) => {
-        const foundProduct = this.products.find(product => product.id === productId);
-
-        if (foundProduct) return foundProduct;
-
-        else console.log("Not found")
+    updateProduct = async (id, updateField) => {
+        const data = await fs.promises.readFile(this.path, 'utf-8')
+        const productDb = await JSON.parse(data)
+        const index = await productDb.findIndex(product => product.id === id)
+        if (index === -1) {
+            return console.log(`No existe producto con el id: ${id}`)
+        }
+        productDb[index] = { ...updateField, id: productDb[index].id }
+        fs.promises.writeFile(this.path, JSON.stringify(productDb, null, '\t'))
+        console.log('Producto actualizado');
     }
-
-
+    deleteProduct = async (id) => {
+        const data = await fs.promises.readFile(this.path, 'utf-8')
+        const productDb = await JSON.parse(data)
+        const index = await productDb.findIndex(product => product.id === id)
+        if (index === -1) {
+            return console.log(`No existe producto con el id: ${id}`)
+        }
+        productDb.splice(index, 1)
+        fs.promises.writeFile(this.path, JSON.stringify(productDb, null, '\t'))
+        console.log('Producto eliminado');
+    }
 }
 
-const productManager = new ProductManager();
 
-productManager.addProduct("producto prueba", "Este  es un producto prueba", 200, "Sin imagen", "abc123", 25)
+const productManager = new ProductManager('./clase5/products.json');
+
+productManager.addProduct({
+    title: "producto prueba",
+    description: "Este  es un producto prueba",
+    price: 200,
+    thumbnail: "Sin imagen",
+    code: 1,
+    stock: 25,
+})
+productManager.addProduct({
+    title: "producto prueba2",
+    description: "Este  es un segundo producto prueba",
+    price: 250,
+    thumbnail: "Sin imagen",
+    code: 2,
+    stock: 30,
+})
